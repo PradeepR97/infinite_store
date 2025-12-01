2190b66252814c18286168b9d6dce177fcbd408a
PradeepR97 <rpradeepkumar976@gmail.com>
2025-12-01 22:41:27 +0530

    Initial commit

---
+++ src/test/java/com/infinitevision/infinite_store/InfiniteStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitevision.infinite_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
